3772dadb0f6c19fab3b2aa5368b13d1e52830ccd
Vladislav Maslinichev <vladislavgmaslinichev@gmail.com>
2023-11-15 11:03:23 +0300

    Revert "Master commit"
    This reverts commit 4d1eac6027cf3ac0a95a38b1ff3f38f99dd409ed.

--- api/exceptions/could_not_read_image_exception.py
+++
@@ -1,7 +0,0 @@
-import PySimpleGUI as sg
-
-
-def check_empty_img(img_model):
-    if img_model is None:
-        sg.popup("Incorrect path")
-        return False
--- api/math_api/density_counter.py
+++
@@ -1,3 +0,0 @@
-def count_density(found_pixels, length_of_one_centimeter):
-    square_centimeter = pow(length_of_one_centimeter, 2)
-    return found_pixels/square_centimeter
